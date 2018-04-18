4499e87224386ae38a8317050b8909b0adb0247b
Max Eden <mx.eden@gmail.com>
2018-04-18 20:51:33 +0300

    GetCalledMethods

---
+++ Mono.Cecil.Fluent/Extensions/MethodDefinition/InstructionsAnalysis.cs
@@ -0,0 +1,30 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable once CheckNamespace
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mono.Cecil.Cil;
+using Mono.Collections.Generic;
+
+namespace Mono.Cecil.Fluent
+{
+    partial class MethodDefinitionExtensions
+    {
+        public static List<MethodReference> GetCalledMethods(this MethodDefinition method)
+        {
+            if (method?.Body?.Instructions == null || method.Body.Instructions.Count == 0)
+            {
+                return new List<MethodReference>();
+            }
+
+            var result = method.Body.Instructions
+                .Where(i => i.OpCode == OpCodes.Call || i.OpCode == OpCodes.Callvirt)
+                .Select(i => (MethodReference)i.Operand)
+                .Distinct()
+                .ToList();
+
+            return result;
+        }
+    }
+}
