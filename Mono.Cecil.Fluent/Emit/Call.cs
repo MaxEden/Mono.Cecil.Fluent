823e9ed25e4a711b8c37d0bed60abc565fdd6cb7
Max Eden <mx.eden@gmail.com>
2018-04-25 05:41:57 +0300

    generic eqCall

--- Mono.Cecil.Fluent/Emit/Call.cs
+++ Mono.Cecil.Fluent/Emit/Call.cs
@@ -1,4 +1,6 @@
-﻿using Mono.Cecil.Cil;
+﻿using System;
+using System.Linq;
+using Mono.Cecil.Cil;
 
 // ReSharper disable once CheckNamespace
 namespace Mono.Cecil.Fluent
@@ -15,6 +17,32 @@ namespace Mono.Cecil.Fluent
             {
                 return Emit(OpCodes.Call, m);
             }
+        }
+
+        public FluentEmitter EqualsCall(TypeDefinition type)
+        {
+            var opEquality = type.Methods.FirstOrDefault(p => p.Name == "op_Equality");
+            var objEquals = Module.SafeImport<object>(p => Object.Equals(null, null)).Resolve();
+
+            if (opEquality != null)
+            {
+                Call(opEquality);
+            }
+            else if (type.IsStruct())
+            {
+                Call(objEquals);
+            }
+            else
+            {
+                Emit(OpCodes.Ceq);
+            }
+
+            return this;
+        }
+
+        public FluentEmitter EqualsStr()
+        {
+            return EqualsCall(Module.TypeSystem.String.Resolve());
         }
     }
 }
