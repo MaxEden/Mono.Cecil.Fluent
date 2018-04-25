5cac17c182512f7f28ad6e430254eead96467b60
Max Eden <mx.eden@gmail.com>
2018-04-25 05:44:11 +0300

    add range extension

--- Mono.Cecil.Fluent/Utils/Generate.cs
+++ Mono.Cecil.Fluent/Utils/Generate.cs
@@ -55,4 +55,15 @@ namespace Mono.Cecil.Fluent.Utils
 
 		}
 	}
+
+    internal static class Extensions
+    {
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> source)
+        {
+            foreach (var item in source)
+            {
+                collection.Add(item);
+            }
+        }
+    }
 }
